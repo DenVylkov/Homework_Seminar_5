57109b0ed8fc2972e1f82ac77b5697baf0c485c1
Ваше имя <@exemple.com>
2022-05-02 14:23:05 +0300

    Version 0.2

--- Seminar_1/Task01/Program.cs
+++ Seminar_1/Task01/Program.cs
@@ -1,6 +1,6 @@
-﻿Console.Write("Введите число A:");
+﻿Console.Write("Введите число A: ");
 int numberA = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите число B:");
+Console.Write("Введите число B: ");
 int numberB = Convert.ToInt32(Console.ReadLine());
 int max = numberA;
 if (numberB > max) max = numberB;
