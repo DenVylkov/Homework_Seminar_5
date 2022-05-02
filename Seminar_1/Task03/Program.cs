cc212a5a27d00548355622a5e9b1bc3dde7e6761
Ваше имя <@exemple.com>
2022-05-02 15:03:39 +0300

    Version 0.1

---
+++ Seminar_1/Task03/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 == 0) 
+{
+    Console.Write("Введеное число четное");
+}
+else
+{
+    Console.Write("Введеное число нечетное");
+} 
+
