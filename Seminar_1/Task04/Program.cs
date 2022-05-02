62159e6c40bf8200c97e2a1feedc7ded8edf3c94
Ваше имя <@exemple.com>
2022-05-02 17:56:44 +0300

    Version 0.3

--- Seminar_1/Task04/Program.cs
+++ Seminar_1/Task04/Program.cs
@@ -11,7 +11,7 @@
 
 Console.WriteLine("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
-for (int i = - number; i <= number; i++)
+for (int i = 1; i <= number; i++)
 {
     Console.Write("");
     if (i % 2 == 0)
