bd4aac98dc4758af1264a541653c6dfa174ca6f8
Ваше имя <@exemple.com>
2022-05-02 14:18:35 +0300

    Done

--- Seminar_1/Task01/Program.cs
+++ Seminar_1/Task01/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число A:");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B:");
+int numberB = Convert.ToInt32(Console.ReadLine());
+int max = numberA;
+if (numberB > max) max = numberB;
+Console.WriteLine("Ответ: ");
+Console.WriteLine(max);
