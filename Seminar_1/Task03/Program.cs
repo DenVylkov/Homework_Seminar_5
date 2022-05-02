b04306d2527c0e639669076dd18aee4303102c61
Ваше имя <@exemple.com>
2022-05-02 15:04:45 +0300

    Version 0.2

--- Seminar_1/Task03/Program.cs
+++ Seminar_1/Task03/Program.cs
@@ -2,10 +2,10 @@
 int number = Convert.ToInt32(Console.ReadLine());
 if (number % 2 == 0) 
 {
-    Console.Write("Введеное число четное");
+    Console.WriteLine("Введеное число четное");
 }
 else
 {
-    Console.Write("Введеное число нечетное");
+    Console.WriteLine("Введеное число нечетное");
 } 
 
