11050b7a3c0196a9bdb6b538f4f5b80a1acef788
Ваше имя <@exemple.com>
2022-05-02 17:39:01 +0300

    Version 01

---
+++ Seminar_1/Task04/Program.cs
@@ -0,0 +1,24 @@
+﻿//Console.Write("Введите число: ");
+//int number = Convert.ToInt32(Console.ReadLine());
+//if (number % 2 == 0) 
+//{
+//    Console.WriteLine("Введеное число: четное");
+//} i % 2 == 0
+//else
+//{
+//    Console.WriteLine("Введеное число: нечетное");
+//} 
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+for (int i = - number; i <= number; i++)
+{
+    Console.Write("");
+    if (i % 2 == 0)
+    {
+    Console.Write(i);
+    Console.Write(", ");
+    }
+
+
+}
