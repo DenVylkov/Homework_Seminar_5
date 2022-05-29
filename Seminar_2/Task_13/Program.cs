fb113cc1e94818dd4b8a9b7e0086268491f30973
Ваше имя <@exemple.com>
2022-05-29 22:04:25 +0300

    Version_2.0

--- Seminar_2/Task_13/Program.cs
+++ Seminar_2/Task_13/Program.cs
@@ -1,18 +1,39 @@
 ﻿/*Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 645 -> 5
 78 -> третьей цифры нет
-32679 -> 6*/
+32679 ->
+Console.WriteLine((Number / 100) % 10);  6*/
 
 
 Console.Clear();
-Console.Write("Введите любое число: ");
+Console.Write("Введите любое положительное число: ");
 int Number = Convert.ToInt32(Console.ReadLine());
-if(Number > 99)
+
+if (Number <= 99)
 {
-    Console.WriteLine((Number / 100) % 10);
+    Console.WriteLine($"В числе {Number} -> третьей цифры нет!");
 }
-else 
+else
 {
-    Console.WriteLine($"{Number} -> третьей цифры нет!");
-    
+    if (Number > 99 & Number <= 999)
+    {
+        int NumberThree = (Number % 10);
+        Console.WriteLine($"{Number} -> {NumberThree}");
+    }
+    if (Number > 999 && Number <= 9999)
+    {
+        int NumberThree = ((Number % 100) / 10);
+        Console.WriteLine($"{Number} -> {NumberThree}");
+    }
+    if (Number > 9999 & Number <= 99999)
+    {
+        int NumberThree = ((Number % 1000) / 100);
+        Console.WriteLine($"{Number} -> {NumberThree}");
+    }
+    if (Number > 99999 & Number <= 999999)
+    {
+        int NumberThree = ((Number % 10000) / 1000);
+        Console.WriteLine($"{Number} -> {NumberThree}");
+    }
+    Console.WriteLine("__________________________");
 }
