e5254565b019bb1a9701db8be752313d2d9cd148
Ваше имя <@exemple.com>
2022-05-30 01:04:10 +0300

    Version_1.1 done

--- Seminar_2/Task_15/Program.cs
+++ Seminar_2/Task_15/Program.cs
@@ -7,12 +7,16 @@
 Console.Clear();
 Console.Write("Введите цифру, обозначающую день недели: ");
 int DayNumber = Convert.ToInt32(Console.ReadLine());
-if (DayNumber >= 1 && DayNumber < 6) 
+if (DayNumber >= 1 && DayNumber <=5) 
 {
     Console.Write($"{DayNumber} -> нет");
 }
-else
+if (DayNumber >= 6 && DayNumber <= 7) 
 {
     Console.Write($"{DayNumber} -> да");
+}
+if (DayNumber > 7)
+{
+  Console.Write("Цифра не является обозначающей день недели!");
 }
 Console.WriteLine();
