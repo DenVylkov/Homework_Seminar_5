aaa3bd8e2b0957dfd95964cbe520d4a90fa1ef25
Ваше имя <@exemple.com>
2022-05-30 00:18:19 +0300

    Version_1.0

---
+++ Seminar_2/Task_15/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+Console.Clear();
+Console.Write("Введите цифру, обозначающую день недели: ");
+int DayNumber = Convert.ToInt32(Console.ReadLine());
+if (DayNumber < 6) 
+{
+    Console.Write($"{DayNumber} -> нет");
+}
+else
+
+    Console.Write($"{DayNumber} -> да");
+
+
+Console.WriteLine();
--- Seminar_2/Task_XXX/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿Console.Clear();
-Console.Write("Введите любое трехзначное число: ");
-int Number = Convert.ToInt32(Console.ReadLine());
-int SecondNumber = (Number % 100) / 10;
-Console.Write($"Ответ: вторая цифра числа {Number} -> {SecondNumber} ");
-Console.WriteLine ();
